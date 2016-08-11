465dd348aa3823fe76b6ad9306da52d842151795
jie <jie.cs@foxmail.com>
2016-08-11 14:00:14 +0800

    update

--- src/me/jie/Leet113.java
+++ src/me/jie/Leet113.java
@@ -4,5 +4,27 @@ package me.jie;
  * Created by jie on 8/8/16.
  * https://leetcode.com/problems/path-sum-ii/
  */
+import java.util.*;
 public class Leet113 {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        if(root == null) return new ArrayList<>();
+        List<List<Integer>> rst = new ArrayList<>();
+        pathLeaf(root, sum, rst, new ArrayList<>());
+        return rst;
+    }
+    private void pathLeaf(TreeNode root, int sum, List<List<Integer>> rst, List<Integer> lt){
+        if(root == null) return;
+        if(root.left == null && root.right == null){
+            if(root.val == sum) {
+                List<Integer> tmp = new ArrayList<>(lt);
+                tmp.add(root.val);
+                rst.add(tmp);
+            }
+            return;
+        }
+        lt.add(root.val);
+        pathLeaf(root.left, sum - root.val, rst, lt);
+        pathLeaf(root.right, sum - root.val, rst, lt);
+        lt.remove(lt.size()-1);
+    }
 }
--- src/me/jie/Leet95.java
+++ src/me/jie/Leet95.java
@@ -4,5 +4,132 @@ package me.jie;
  * Created by jie on 8/8/16.
  * https://leetcode.com/problems/unique-binary-search-trees-ii/
  */
+import javafx.util.Pair;
+
+import java.util.*;
 public class Leet95 {
+    /*
+       求(1, n), 遍历每个数,分别为根
+       然后转成求  1 -> (2, n),  (1) <- 2 -> (3, n), ...
+       保存中间结果 Map (i, j, root)
+     */
+    public class Solution {
+        Map<Pair<Integer, Integer>, List<TreeNode>> mp;
+        List<TreeNode> rst;
+        public List<TreeNode> generateTrees(int n) {
+            if(n == 0) return new ArrayList<>();
+            mp = new HashMap<>();
+            rst = gTrees(1, n);
+            return rst;
+
+        }
+
+        private List<TreeNode> gTrees(int begin, int end){
+            Pair<Integer, Integer> p = new Pair<>(begin, end);
+            if(mp.containsKey(p)){
+                return mp.get(p);
+            }
+            else {
+                List<TreeNode> lt;
+                if (begin >= end) {
+                    TreeNode node;
+                    lt = new ArrayList<>();
+                    if (begin > end) {
+                        node = null;
+                    } else {
+                        node = new TreeNode(begin);
+                        node.left = null;
+                        node.right = null;
+                    }
+                    lt.add(node);
+                    mp.put(p, lt);
+                    return lt;
+                }
+
+                List<TreeNode> left, right;
+                TreeNode root;
+                Pair<Integer, Integer> pair1, pair2;
+                lt = new ArrayList<>();
+                for (int i = begin; i <= end; i++) {
+
+                    pair1 = new Pair<>(begin, i - 1);
+                    if (!mp.containsKey(pair1)) {
+                        left = gTrees(begin, i - 1);
+                        mp.put(pair1, left);
+                    } else {
+                        left = mp.get(pair1);
+                    }
+                    pair2 = new Pair<>(i + 1, end);
+                    if (!mp.containsKey(pair2)) {
+                        right = gTrees(i + 1, end);
+                        mp.put(pair2, right);
+                    } else {
+                        right = mp.get(pair2);
+                    }
+                    for (TreeNode l : left) {
+                        for (TreeNode r : right) {
+                            root = new TreeNode(i);
+                            root.left = l;
+                            root.right = r;
+                            lt.add(root);
+                        }
+                    }
+
+                }
+                mp.put(p, lt);
+                return lt;
+            }
+        }
+    }
+
+    /*
+        不保存中间结果, 小数据量
+     */
+    public class Solution2{
+        public List<TreeNode> generateTrees(int n) {
+            if(n == 0) return new ArrayList<>();
+            List<TreeNode> rst = gTrees(1, n);
+            return rst;
+        }
+        private List<TreeNode> gTrees(int begin, int end){
+            List<TreeNode> lt = new ArrayList<>();
+            if(begin > end){
+                lt.add(null);
+                return lt;
+            }
+            if(begin == end){
+                lt.add(new TreeNode(begin));
+                return lt;
+            }
+            for(int i = begin; i <= end; i ++){
+                List<TreeNode> left = gTrees(begin, i - 1);
+                List<TreeNode> right = gTrees(i + 1, end);
+                for(TreeNode l : left){
+                    for(TreeNode r : right){
+                        TreeNode root = new TreeNode(i);
+                        root.left = l;
+                        root.right = r;
+                        lt.add(root);
+                    }
+                }
+            }
+            return lt;
+        }
+    }
+    Solution s1 = new Solution();
+    Solution2 s2 = new Solution2();
+    private void test(){
+        int  n = 13;
+        long t = System.currentTimeMillis();
+        s1.generateTrees(n);
+        System.out.println(System.currentTimeMillis() - t);
+
+        long t1 = System.currentTimeMillis();
+        s2.generateTrees(n);
+        System.out.println(System.currentTimeMillis() - t1);
+
+    }
+    public static void main(String[] args){
+        new Leet95().test();
+    }
 }
